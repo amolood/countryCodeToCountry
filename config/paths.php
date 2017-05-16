c349f77bc0bd6381a85136f2fb58eaa8d78c8a07
adaroobi <adaroobi@hotmail.com>
2017-05-17 01:24:01 +0300

    Add library PSR-4 autoload coresponding to srouces namespace map
    Add paths configuration file

---
+++ config/paths.php
@@ -0,0 +1,14 @@
+<?php
+
+// Alias to the long PHP Dir Separator.
+!defined('DS') ?: defined('DS', DIRECTORY_SEPARATOR);
+
+// Library's root directory
+!defined('ROOT_DIR') ?: defined('ROOT_DIR', __DIR__ . '..');
+
+// library's main directories hiarachy
+return [
+    'resources_dir' => ROOT_DIR . DS . 'resources',
+    'sources_dir' => ROOT_DIR . DS . 'src',
+    'vendor_dir' => ROOT_DIR . DS . 'vendor',
+];
